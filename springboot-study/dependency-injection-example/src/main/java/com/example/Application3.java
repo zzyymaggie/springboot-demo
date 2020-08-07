f88ff0be6630d2c135d1ee7668068446302be2db
zhangyu20 <zhangyu20@sunlands.com>
2020-08-07 20:31:49 +0800

    更正一下：这种情况下注入是正常的

--- springboot-study/dependency-injection-example/src/main/java/com/example/Application3.java
+++ springboot-study/dependency-injection-example/src/main/java/com/example/Application3.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 测试@Value注入的生命周期
- * { @Value }和{ @Autowired} 都是由AutowiredAnnotationBeanPostProcessor解析注入，发生在populateBean时期，
- * 所以实例化阶段无法获取到注入的值
- */
 @RestController
 @SpringBootApplication
 public class Application3 {
