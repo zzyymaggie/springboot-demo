56d9cd3eb113e8a033eec99e4bb3fa2c2c5db9a1
zhangyu20 <zhangyu20@sunlands.com>
2020-08-08 18:35:42 +0800

    优化代码ConfigurationProperties类使用

--- springboot-study/dependency-injection-example/src/main/java/com/example/fixed/HelloProperties.java
+++ springboot-study/dependency-injection-example/src/main/java/com/example/fixed/HelloProperties.java
@@ -3,12 +3,15 @@ package com.example.fixed;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "hello")
 public class HelloProperties {
-    @Value("${hello.log}")
     private boolean log;
 
     public boolean isLog() {
         return log;
+    }
+
+    public void setLog(boolean log) {
+        this.log = log;
     }
 }
