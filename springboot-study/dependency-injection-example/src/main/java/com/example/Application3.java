36f7f96fe11a23c915c2fc02d213e4abdc8559a5
zhangyu20 <zhangyu20@sunlands.com>
2020-08-08 16:56:40 +0800

    优化代码，添加容器启动完毕后打印配置类属性

--- springboot-study/dependency-injection-example/src/main/java/com/example/Application3.java
+++ springboot-study/dependency-injection-example/src/main/java/com/example/Application3.java
@@ -1,14 +1,18 @@
 package com.example;
+
+import com.example.issue.MyConfiguration2;
 import com.example.util.MyConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class Application3 {
+public class Application3 implements ApplicationListener<ContextRefreshedEvent> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application3.class, args);
@@ -17,8 +21,23 @@ public class Application3 {
 	@Autowired
 	private MyConfiguration myConfiguration;
 
+	@Autowired
+	private MyConfiguration2 myConfiguration2;
+
 	@GetMapping("/test")
 	public String test() {
-		return "controller: " + myConfiguration.isLog();
+		return "test: " + myConfiguration.isLog();
+	}
+
+
+	@GetMapping("/test2")
+	public String test2() {
+		return "test2:" + myConfiguration2.isLog();
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		System.out.println("myConfiguration.isLog:" + myConfiguration.isLog());
+		System.out.println("myConfiguration2.isLog:" + myConfiguration2.isLog());
 	}
 }
